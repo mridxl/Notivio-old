79b15e6303fa8952495b0db55c03a500d1458292
Mridul Khandelwal <khandelwalmridul03@gmail.com>
2024-09-15 18:14:29 +0530

    fix: add try catch

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -10,31 +10,35 @@ export default async function (req, res) {
 	if (!parsedPayload.success) {
 		return res.status(400).json({ error: zodMessage(parsedPayload) });
 	}
+	try {
+		const { email, password } = parsedPayload.data;
+		const user = await User.findOne({ 'personal_info.email': email }).exec();
 
-	const { email, password } = parsedPayload.data;
-	const user = await User.findOne({ 'personal_info.email': email }).exec();
+		if (!user) {
+			return res.status(400).json({ error: 'Invalid email or password' });
+		}
+		const isPasswordValid = await bcrypt.compare(
+			password,
+			user.personal_info.password
+		);
 
-	if (!user) {
-		return res.status(400).json({ error: 'Invalid email or password' });
+		if (!isPasswordValid) {
+			return res.status(400).json({ error: 'Invalid email or password' });
+		}
+
+		const token = jwt.sign({ id: User._id }, process.env.JWT_SECRET);
+		res.cookie('token', token, {
+			httpOnly: true,
+			sameSite: 'strict',
+			maxAge: 1000 * 60 * 60 * 24 * 15, // 15 days
+		});
+
+		return res.status(200).json({
+			message: 'User logged in successfully',
+			user: formatUserResponse(user),
+		});
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json({ error: 'Something went wrong' });
 	}
-	const isPasswordValid = await bcrypt.compare(
-		password,
-		user.personal_info.password
-	);
-
-	if (!isPasswordValid) {
-		return res.status(400).json({ error: 'Invalid email or password' });
-	}
-
-	const token = jwt.sign({ id: User._id }, process.env.JWT_SECRET);
-	res.cookie('token', token, {
-		httpOnly: true,
-		sameSite: 'strict',
-		maxAge: 1000 * 60 * 60 * 24 * 15, // 15 days
-	});
-
-	return res.status(200).json({
-		message: 'User logged in successfully',
-		user: formatUserResponse(user),
-	});
 }
