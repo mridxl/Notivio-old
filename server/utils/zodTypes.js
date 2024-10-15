3318efa0400f27d0c94b17c344f1506f738ea0b9
Mridul Khandelwal <khandelwalmridul03@gmail.com>
2024-10-15 11:51:39 +0530

    Fix bug: Publish blog schema

--- server/utils/zodTypes.js
+++ server/utils/zodTypes.js
@@ -20,14 +20,8 @@ export const blogSchema = z.object({
 	title: z.string().min(3, 'Title must be at least 3 characters long'),
 	des: z.string().max(200, 'Description must be at most 200 characters long'),
 	content: z.object({
-		blocks: z.array(
-			z.object({
-				type: z.string(),
-				data: z.object({
-					text: z.string(),
-				}),
-			})
-		),
+		time: z.number(),
+		blocks: z.array(z.object({})),
 	}),
 	tags: z.array(z.string()).max(10, 'Maximum 10 tags are allowed'),
 	banner: z.string(),
