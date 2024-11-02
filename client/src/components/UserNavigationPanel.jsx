d4d04baee0dc59e0560844db46d2e4c4bfbffd25
Mridul Khandelwal <khandelwalmridul03@gmail.com>
2024-11-02 23:57:10 +0530

    Update UserNavigationPanel component

--- client/src/components/UserNavigationPanel.jsx
+++ client/src/components/UserNavigationPanel.jsx
@@ -35,16 +35,16 @@ function UserNavigationPanel() {
 					<i className="fi fi-rr-edit"></i>
 					<p>Write</p>
 				</Link>
-				<Link to={`/user/${username}`} className="link pl-8 py-4 md:hidden">
+				<Link to={`/user/${username}`} className="link pl-8 py-4">
 					Profile
 				</Link>
-				<Link to="/dashboard/blogs" className="link pl-8 py-4 md:hidden">
+				<Link to="/dashboard/blogs" className="link pl-8 py-4">
 					Dashboard
 				</Link>
-				<Link to="/settings/edit-profile" className="link pl-8 py-4 md:hidden">
+				<Link to="/settings/edit-profile" className="link pl-8 py-4">
 					Settings
 				</Link>
-				<span className="absolute border-t border-grey w-[100%] md:hidden"></span>
+				<span className="absolute border-t border-grey w-[100%]"></span>
 				<button
 					className="text-left p-4 hover:bg-grey w-full pl-8"
 					onClick={handleLogout}
