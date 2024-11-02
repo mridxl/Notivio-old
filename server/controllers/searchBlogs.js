081ab838568c85e66250d1b3d3071f4b59bfac86
Mridul Khandelwal <khandelwalmridul03@gmail.com>
2024-11-02 23:53:46 +0530

    Add author search to searchBlogs controller

--- server/controllers/searchBlogs.js
+++ server/controllers/searchBlogs.js
@@ -1,13 +1,26 @@
 import Blog from '../models/Blog.js';
+import User from '../models/User.js';
 
 export default async function searchBlogs(req, res) {
-	const { tag, query, page } = req.body;
+	const { tag, query, page, author } = req.body;
 	let findQuery;
 
 	if (tag) {
 		findQuery = { tags: { $in: [tag.toLowerCase()] }, draft: 'false' };
-	} else {
+	} else if (query) {
 		findQuery = { title: new RegExp(query, 'i'), draft: 'false' };
+	} else if (author) {
+		const user = await User.findOne({
+			'personal_info.username': author,
+		});
+
+		if (!user) {
+			return res.status(404).json({ error: 'User not found' });
+		}
+
+		findQuery = { author: user._id, draft: 'false' };
+	} else {
+		return res.status(400).json({ error: 'Invalid Request' });
 	}
 
 	const maxLimit = 5;
