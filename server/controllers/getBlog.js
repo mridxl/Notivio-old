63300da314dd7787f00f26dbcaa6b22bc61acd61
Mridul Khandelwal <khandelwalmridul03@gmail.com>
2024-11-12 19:56:14 +0530

    add is editing check in get-blogs

--- server/controllers/getBlog.js
+++ server/controllers/getBlog.js
@@ -2,18 +2,21 @@ import Blog from '../models/Blog.js';
 import User from '../models/User.js';
 
 export default async function getBlog(req, res) {
-	const { id } = req.query;
+	const { id, mode, draft } = req.query;
 	if (!id) {
 		return res.status(400).json({ error: 'Invalid Request' });
 	}
 	if (!/^[a-zA-Z0-9-]*$/.test(id)) {
 		return res.status(400).json({ error: 'Invalid Request' });
 	}
+
+	const incrementVal = mode && mode === 'edit' ? 0 : 1;
+
 	try {
 		const blog = await Blog.findOneAndUpdate(
 			{ blog_id: id },
 			{
-				$inc: { 'activity.total_reads': 1 },
+				$inc: { 'activity.total_reads': incrementVal },
 			}
 		)
 			.populate(
@@ -29,9 +32,12 @@ export default async function getBlog(req, res) {
 		await User.findOneAndUpdate(
 			{ 'personal_info.username': blog.author.personal_info.username },
 			{
-				$inc: { 'activity_info.total_reads': 1 },
+				$inc: { 'activity_info.total_reads': incrementVal },
 			}
 		);
+		if (blog.draft && !draft) {
+			return res.status(404).json({ error: 'Blog not found' });
+		}
 		return res.status(200).json({ blog });
 	} catch (err) {
 		console.log(err);
